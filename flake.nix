1f87e67244a3b84b74a78ca94ab4f049c404398d
James Hobson <james@hobson.space>
2026-04-16 20:22:28 +0200

    flaked!

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+
+    packages.default = pkgs.haskellPackages.callCabal2nix "forth-hs" ./. {} ;
+
+  });
+}
